7ca00048dff9b6b25bb94c97547ebb64ca1fd7c0
huyhuync2937 <nguyenhuync2937@gmail.com>
2024-10-18 09:17:44 +0700

    add

---
+++ New folder/contracts/utils/Address.js
@@ -0,0 +1,7 @@
+import { SMART_ADDRESS } from "../Common";
+
+export const getUSDAddress = () => SMART_ADDRESS.USDAddress;
+export const getTokenAddress = () => SMART_ADDRESS.TokenAddress;
+export const getPoolAddress = () => SMART_ADDRESS.PoolAddress;
+export const getPoolFactory = () => SMART_ADDRESS.PoolFactoryAddress;
+export const getVaultFactory = () => SMART_ADDRESS.VaultAddress;
